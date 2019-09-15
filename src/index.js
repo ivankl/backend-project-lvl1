7417383969931c789c7dec02777d18e7530dd27c
ivankl <ivan@roonlabs.com>
2019-09-15 23:41:41 +0300

    Formatting changes for index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import readLineSync from 'readline-sync';
 
-export function user() { 
-	var userName = readLineSync.question('May I have your name? ');
-	console.log('Hi, ' + userName + '!');
-	return;
+export default function user() {
+  const userName = readLineSync.question('May I have your name? ');
+  console.log(`Hi, ${userName}!`);
 }
