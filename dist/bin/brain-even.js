4bdfde3685d59bdcf82a53e79bd74048380ee532
ivankl <ivan@roonlabs.com>
2019-09-19 01:38:24 +0300

    Changes to package.json 1.2

--- dist/bin/brain-even.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-
-var _ = require("..");
-
-(0, _.brainGame)();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZXZlbi5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTs7O0FBQ0E7O0FBRUEiLCJzb3VyY2VzQ29udGVudCI6WyIjIS91c3IvYmluL2VudiBub2RlXG5pbXBvcnQgeyBicmFpbkdhbWUgfSBmcm9tICcuLic7XG5cbmJyYWluR2FtZSgpO1xuIl19
