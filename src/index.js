c2d5a22ae57a8c547798bae715397bb5cc4b6144
ivankl <ivan@roonlabs.com>
2019-11-21 01:26:31 +0300

    random number generator fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ export const user = () => {
   return userName;
 };
 
-export const getRandomNumber = () => Math.floor(Math.random() * Math.floor(50));
+export const getRandomNumber = () => {
+  const min = 5;
+  const max = 30;
+  return (Math.floor(Math.random() * (max - min + 1)) + min);
+};
 
 export const userReplyCheck = (expectedAnswer, name) => {
   const reply = readLineSync.question('Your answer: ').toLowerCase();
