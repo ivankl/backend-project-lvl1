83d4ecef218443da568849b9532ee7c733b0f4a8
ivankl <ivan@roonlabs.com>
2019-11-26 10:54:34 +0300

    element number change

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,21 +1,22 @@
 import { getRandomNumber, brainGame } from '../index';
 
 
-const displayProgression = (string, index, missingElement, currentElement, step) => {
-  if (index > 9) {
+const displayProgression = (string, index, missingElement, currentElement, step, elementAmount) => {
+  if (index > elementAmount) {
     return string;
   }
   if (index === missingElement) {
-    return displayProgression(`${string} .. `, index + 1, missingElement, currentElement + step, step);
+    return displayProgression(`${string} .. `, index + 1, missingElement, currentElement + step, step, elementAmount);
   }
-  return displayProgression(`${string} ${currentElement}`, index + 1, missingElement, currentElement + step, step);
+  return displayProgression(`${string} ${currentElement}`, index + 1, missingElement, currentElement + step, step, elementAmount);
 };
 
 const valueOfSkippedNumber = () => {
+  const progressionLength = 9;
   const initialNumber = getRandomNumber();
   const step = getRandomNumber();
-  const location = Math.floor(Math.random() * Math.floor(9));
-  return { result: (initialNumber + location * step), question: `${displayProgression('', 0, location, initialNumber, step)}` };
+  const location = Math.floor(Math.random() * Math.floor(progressionLength));
+  return { result: (initialNumber + location * step), question: `${displayProgression('', 0, location, initialNumber, step, progressionLength)}` };
 };
 
 export const progressionGame = () => brainGame(valueOfSkippedNumber, '\nWhat number is missing in the progression?');
