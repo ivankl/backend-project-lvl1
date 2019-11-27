9d98ade59fc89e19b858081b267fdb702a74208a
ivankl <ivan@roonlabs.com>
2019-11-27 21:47:28 +0300

    code refactoring 3.1

--- src/games/even.js
+++ src/games/even.js
@@ -3,8 +3,8 @@ import { getRandomNumber, brainGame } from '../index';
 const isEven = (number) => (number % 2 === 0);
 
 const evenCheck = () => {
-  const currentNumber = getRandomNumber();
-  return { result: (isEven(currentNumber) ? 'yes' : 'no'), question: `${currentNumber}` };
+  const questionNumber = getRandomNumber();
+  return { result: (isEven(questionNumber) ? 'yes' : 'no'), question: `${questionNumber}` };
 };
 
 const gameRules = '\nAnswer "yes" if the number is even, otherwise answer "no".';
--- src/games/progression.js
+++ src/games/progression.js
@@ -3,11 +3,11 @@ import { getRandomNumber, brainGame } from '../index';
 const progressionLength = 10;
 
 const displayProgression = (string, index, missingElement, currentElement, step, elementAmount) => {
-  if (index > elementAmount) {
+  if (index > elementAmount - 1) {
     return string;
   }
   if (index === missingElement) {
-    return displayProgression(`${string} .. `, index + 1, missingElement, currentElement + step, step, elementAmount);
+    return displayProgression(`${string} ..`, index + 1, missingElement, currentElement + step, step, elementAmount);
   }
   return displayProgression(`${string} ${currentElement}`, index + 1, missingElement, currentElement + step, step, elementAmount);
 };
@@ -15,7 +15,7 @@ const displayProgression = (string, index, missingElement, currentElement, step,
 const valueOfSkippedNumber = () => {
   const firstElement = getRandomNumber();
   const step = getRandomNumber();
-  const location = getRandomNumber(progressionLength);
+  const location = getRandomNumber(progressionLength - 1);
   return { result: (firstElement + location * step), question: `${displayProgression('', 0, location, firstElement, step, progressionLength)}` };
 };
 
